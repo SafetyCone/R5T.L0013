2d0c2c378b93fb114899a7efe6758994ae1f44a8
davidcoats <david@getsafetycone.com>
2021-10-22 13:28:38 -0700

    Initial commit of source-stub.

---
+++ source/R5T.L0013.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0013.Construction
+{
+    /// <summary>
+    /// Construction project for Microsoft.Extensions.DependencyInjection Nuget package selector..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.L0013/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.L0013
+{
+    public class Class1
+    {
+    }
+}
